6c0bdfc080f17d939169d0bea9eb3879c6ec8a5f
Mehul-clou <mehulgithub@gmail.com>
2021-09-14 17:33:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# print(ord('z'))
+str1 ="how are you? i am fine. where do you live?"
+# new_element = ""
+# output = ""
+# new_list = str1.split()
+# # print(new_list)
+# for i in range(len(new_list)):
+#     new_char = new_list[int(i)]
+#     if ord(new_char[0])>=97 and ord(new_char[0])<=122:
+#         new_element = chr(ord(new_char[0])-32)
+#         new_element += new_char[1:]
+#     output = output + new_element +" "
+#
+# print(output)
+
+print(str1.title())
